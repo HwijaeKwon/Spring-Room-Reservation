4e60fbf51c697eefcd9e24d9dbce057d419ec3d6
hwijae_kwon <hwijae_kwon@tmax.co.kr>
2021-06-01 02:42:28 +0900

    [feat] 디렉토리 구조 변경
    - room, reservation, user 별로 디렉토리 구분

--- src/main/java/study/practice/domain/repository/room/MeetingRoomRepository.java
+++ src/main/java/study/practice/domain/repository/room/MeetingRoomRepository.java
@@ -1,4 +1,4 @@
-package study.practice.domain.repository;
+package study.practice.domain.repository.room;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.practice.domain.model.MeetingRoom;
--- src/main/java/study/practice/domain/service/room/RoomService.java
+++ src/main/java/study/practice/domain/service/room/RoomService.java
@@ -1,10 +1,12 @@
-package study.practice.domain.service;
+package study.practice.domain.service.room;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import study.practice.domain.model.MeetingRoom;
 import study.practice.domain.model.ReservableRoom;
-import study.practice.domain.repository.ReservableRoomRepository;
+import study.practice.domain.repository.room.MeetingRoomRepository;
+import study.practice.domain.repository.room.ReservableRoomRepository;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -16,7 +18,15 @@ public class RoomService {
     @Autowired
     ReservableRoomRepository roomRepository;
 
+    @Autowired
+    MeetingRoomRepository meetingRoomRepository;
+
     public List<ReservableRoom> findReservableRooms(LocalDate date) {
         return roomRepository.findByReservableRoomId_reservedDateOrderByReservableRoomId_roomIdAsc(date);
+    }
+
+    public MeetingRoom findMeetingRoom(Integer roomId) {
+        return meetingRoomRepository.findById(roomId)
+                .orElseThrow(() -> new IllegalStateException("회의실을 찾을 수 없습니다"));
     }
 }
