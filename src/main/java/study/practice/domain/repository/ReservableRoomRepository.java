192b29c48dd8d428ec387c1992203a8dd52e4500
hwijae_kwon <hwijae_kwon@tmax.co.kr>
2021-06-01 00:19:45 +0900

    [feat] ReservableRoomRepository 구현 - JpaRepository를 상속하여 구현함 - 현재는 function name을 이용하여 작성하였는데 보다 좋은 가독성을 위해 NamedQuery나 custom repository를 만드는 것이 좋을 것 같다

---
+++ src/main/java/study/practice/domain/repository/ReservableRoomRepository.java
@@ -0,0 +1,12 @@
+package study.practice.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.practice.domain.model.ReservableRoom;
+import study.practice.domain.model.ReservableRoomId;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ReservableRoomRepository extends JpaRepository<ReservableRoom, ReservableRoomId> {
+    List<ReservableRoom> findByReservableRoomId_reservedDateOrderByReservableRoomId_roomIdAsc(LocalDate reservedDate);
+}
