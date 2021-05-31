c4716fecfd39a9168ccc2ae5298873d1ea4c661a
hwijae_kwon <hwijae_kwon@tmax.co.kr>
2021-06-01 00:50:44 +0900

    [feat] RoomController 메서드 추가
    - PathVariable로 date를 받는 핸들러 메서드를 추가함

--- src/main/java/study/practice/app/room/Room.java
+++ src/main/java/study/practice/app/room/Room.java
@@ -1,11 +1,12 @@
 package study.practice.app.room;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 import study.practice.domain.model.ReservableRoom;
 import study.practice.domain.service.RoomService;
 
@@ -22,8 +23,15 @@ public class Room {
     @RequestMapping(method = RequestMethod.GET)
     String listRooms(Model model) {
         LocalDate today = LocalDate.now();
-        List<ReservableRoom> rooms = roomService.findReservableRooms(today);
-        model.addAttribute("date", today);
+        return listRooms(today, model);
+    }
+
+    @RequestMapping(value = "/{date}", method = RequestMethod.GET)
+    String listRooms(
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @PathVariable("date") LocalDate date,
+            Model model) {
+        List<ReservableRoom> rooms = roomService.findReservableRooms(date);
+        model.addAttribute("date", date);
         model.addAttribute("rooms", rooms);
         return "room/listRooms";
     }
