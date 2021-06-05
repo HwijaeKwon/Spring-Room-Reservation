1f665bd6063c00be41aa845a3a43e1cf8769f705
hwijae_kwon <hwijae_kwon@tmax.co.kr>
2021-06-06 03:11:14 +0900

    [feat] ReservableRoomRepository에 NamedQuery적용
    - ReservableRoomRepository에 NamedQuery 적용
    - fetch join을 적용하여 query수를 줄임 (N + 1 problem 개선)

--- src/main/java/study/practice/domain/repository/room/ReservableRoomRepository.java
+++ src/main/java/study/practice/domain/repository/room/ReservableRoomRepository.java
@@ -2,6 +2,8 @@ package study.practice.domain.repository.room;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.practice.domain.model.ReservableRoom;
 import study.practice.domain.model.ReservableRoomId;
 
@@ -11,7 +13,12 @@ import java.util.List;
 
 public interface ReservableRoomRepository extends JpaRepository<ReservableRoom, ReservableRoomId> {
 
-    List<ReservableRoom> findByReservableRoomId_reservedDateOrderByReservableRoomId_roomIdAsc(LocalDate reservedDate);
+    //복합키가 reserveDate, roomId 순으로 정렬됨 -> where로 빠르게 조회하고, order로 빠르게 정렬할 수 있다
+    @Query("SELECT DISTINCT r FROM ReservableRoom r "
+            + "LEFT JOIN FETCH r.meetingRoom "
+            + "WHERE r.reservableRoomId.reserveDate = :date "
+            + "ORDER BY r.reservableRoomId.roomId ASC")
+    List<ReservableRoom> findByReservableRoomId_reservedDateOrderByReservableRoomId_roomIdAsc(@Param("date") LocalDate reservedDate);
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     ReservableRoom findOneForUpdateByReservableRoomId(ReservableRoomId reservableRoomId);
